b0b31b62355020a25746d92da304dcc25a29d102
parksaneon <sangeon1500@daum.net>
2021-11-03 11:12:18 +0900

    feat : nickname submit

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -1,26 +1,42 @@
 const messageList = document.querySelector('ul');
-const messageForm = document.querySelector('form');
+const nickForm = document.querySelector('#nick');
+const messageForm = document.querySelector('#message');
 // 사용자의 정보를 소켓이 담는다.
 // socket은 서버로의 연결은 뜻한다.
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
 
 socket.addEventListener('open', () => {
   console.log('Connected to Browser');
 });
 
 socket.addEventListener('message', (message) => {
-  console.log(message.data);
+  const li = document.createElement('li');
+  li.innerText = message.data;
+  messageList.append(li);
 });
 
 socket.addEventListener('close', () => {
   console.log('Disconnected from server');
 });
 
-function handleSubmit(event) {
+function handleNickSubmit(event) {
   event.preventDefault();
-  const input = messageForm.querySelector('input');
-  socket.send(input.value);
+  const input = nickForm.querySelector('input');
+  socket.send(makeMessage('nickname', input.value));
   input.value = '';
 }
 
-messageForm.addEventListener('submit');
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector('input');
+  socket.send(makeMessage('new_message', input.value));
+  input.value = '';
+}
+
+nickForm.addEventListener('submit', handleNickSubmit);
+messageForm.addEventListener('submit', handleSubmit);
