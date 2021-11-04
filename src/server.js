00463322018d513271ab0a05f890fdca38d9f040
parksaneon <sangeon1500@daum.net>
2021-11-04 11:07:21 +0900

    install socket.io

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import http from 'http';
-import WebSocket from 'ws';
+import SocketIO from 'socket.io';
+// import WebSocket from 'ws';
 import express from 'express';
 
 const app = express();
@@ -15,36 +16,33 @@ app.get('/*', (req, res) => res.redirect('/'));
 const handleListen = console.log('Listening on http://localhost:3000');
 
 // http server
-const server = http.createServer(app);
+const httpServer = http.createServer(app);
+const wsServer = SocketIO(httpServer);
 
 // websocket server
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
 
-const sockets = [];
+// const sockets = [];
+// // 콜백함수의 socket 매개변수는 연결된 브라우저를 뜻한다.
+// wss.on('connection', (socket) => {
+//   sockets.push(socket);
+//   socket['nickname'] = 'Anon';
+//   console.log('connected to Browser');
 
-// 콜백함수의 socket 매개변수는 연결된 브라우저를 뜻한다.
-wss.on('connection', (socket) => {
-  sockets.push(socket);
-  socket['nickname'] = 'Anon';
+//   socket.on('close', () => {
+//     console.log('Disconnected from Browser');
+//   });
 
-  console.log('connected to Browser');
-
-  socket.on('close', () => {
-    console.log('Disconnected from Browser');
-  });
-
-  socket.on('message', (msg) => {
-    const [type, payload] = JSON.parse(msg);
-
-    switch (type) {
-      case 'new_message':
-        sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${payload}`));
-      case 'nickname':
-        socket['nickname'] = payload;
-    }
-  });
-
-  socket.send('hello');
-});
+//   socket.on('message', (msg) => {
+//     const [type, payload] = JSON.parse(msg);
+//     switch (type) {
+//       case 'new_message':
+//         sockets.forEach((aSocket) => aSocket.send(`${socket.nickname} : ${payload}`));
+//       case 'nickname':
+//         socket['nickname'] = payload;
+//     }
+//   });
+//   socket.send('hello');
+// });
 
 app.listen(3000, handleListen);
