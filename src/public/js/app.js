42f0fc0c65fb0a522f7e3aa8566c6b8d6a97cac8
parksaneon <sangeon1500@daum.net>
2021-11-10 10:50:34 +0900

    feat : mute, camera eventHandler

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,6 +5,8 @@ const muteBtn = document.getElementById('mute');
 const cameraBtn = document.getElementById('camera');
 
 let myStream;
+let muted = false;
+let cameraOff = false;
 
 async function getMedia() {
   try {
@@ -12,7 +14,31 @@ async function getMedia() {
       audio: true,
       video: true,
     });
+    myFace.srcObject = myStream;
   } catch (e) {
     console.log(e);
   }
 }
+
+function handleMuteClick() {
+  if (!muted) {
+    muteBtn.innerText = 'Unmute';
+    muted = true;
+  } else {
+    muteBtn.innerText = 'Mute';
+    muted = false;
+  }
+}
+
+function handleCameraClick() {
+  if (cameraOff) {
+    cameraBtn.innerText = 'Turn Camera Off';
+    cameraOff = false;
+  } else {
+    cameraBtn.innerText = 'Turn Camera On';
+    cameraOff = true;
+  }
+}
+
+muteBtn.addEventListener('click', handleMuteClick);
+cameraBtn.addEventListener('click', handleCameraClick);
